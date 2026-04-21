23cb0111d4fddb43d105391a97090d442c860c66
nbald <lolbalda@gmail.com>
2026-04-21 15:27:32 +0200

    fix: ripristinato useState mancante nell'import di LibraryManagementView

--- frontend/components/LibraryManagementView.tsx
+++ frontend/components/LibraryManagementView.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {observer} from 'mobx-react-lite';
 import {mediaStore} from '../store/mediaStore.ts';
 import {
